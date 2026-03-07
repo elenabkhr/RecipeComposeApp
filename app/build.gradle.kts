46f377bf40c6b72b9128a38c52e81507d35fadaf
lena <lll.00088@mail.ru>
2026-03-07 19:56:32 +0300

    RCA-16: Внедрить Jetpack Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
